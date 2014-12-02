ae921cbfd6af7dae22a182b4fc0580c61e1ac1f5
wolske <st4rbux@gmail.com>
2014-12-02 17:17:59 -0500

    set Epoch Time and pszTimestamp; ready to generate Merkel Hash

--- src/main.cpp
+++ src/main.cpp
@@ -2779,7 +2779,7 @@ bool InitBlockIndex() {
         //   vMerkleTree: 97ddfbbae6
 
         // Genesis block
-        const char* pszTimestamp = "NY Times 05/Oct/2011 Steve Jobs, Apple’s Visionary, Dies at 56";
+        const char* pszTimestamp = "NY Times 2014-12-02: Obama Offers New Standards on Police Gear";
         CTransaction txNew;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
@@ -2791,13 +2791,13 @@ bool InitBlockIndex() {
         block.hashPrevBlock = 0;
         block.hashMerkleRoot = block.BuildMerkleTree();
         block.nVersion = 1;
-        block.nTime    = 1317972665;
+        block.nTime    = 1417558370;
         block.nBits    = 0x1e0ffff0;
         block.nNonce   = 2084524493;
 
         if (fTestNet)
         {
-            block.nTime    = 1317798646;
+            block.nTime    = 1417558370;
             block.nNonce   = 385270584;
         }
 
